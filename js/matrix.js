3937896d93fa4f3fb9a68a588a43d8368ee0a8ce
Daniel Chernis <32421770+dch133@users.noreply.github.com>
2018-01-03 20:10:45 -0500

    better matrix effects

--- js/matrix.js
+++ js/matrix.js
@@ -1,6 +1,6 @@
 var file = document.getElementById("file");
 
-//Blinking image
+//Blinking image (icon)
 var blink = window.setInterval(function blinking() {
   if (file.style.visibility == 'hidden') {
     file.style.visibility = 'visible';
@@ -30,15 +30,15 @@ function rotateAnimation() {
 var zoom;
 var initWidth = img.clientWidth;
 var initHeight = img.clientHeight;
-var bool = true;
+
 function zooming() {
   zoom = setInterval(function () {
     var initWidth = img.clientWidth;
     var initHeight = img.clientHeight;
-    img.style.width = (initWidth + 10) + "px";
-    img.style.height = (initHeight + 10) + "px";
+    img.style.width = (initWidth + 20) + "px";
+    img.style.height = (initHeight + 20) + "px";
 
-  }, 25);
+  }, 40);
 
 }
 
@@ -46,9 +46,20 @@ function zooming() {
 function move() {
   var elem = document.getElementById("myBar");
   var barLength = 0;
-  var bar = setInterval(frame, 50);
+  var bar = setInterval(frame, 40);
   var width = img.clientWidth;
   var height = img.clientHeight;
+
+  $("h5").css('visibility', 'hidden');
+  $(".login").html("Uploading...")
+  $("header").css('visibility', 'hidden');
+  $("footer").css('visibility', 'hidden');
+  $("body").css({
+    'background-size': 'cover',
+    'background-image': 'url(../img/matrix/matrix2.jpg)',
+    'background-repeat': 'no-repeat'
+  });
+
 
   rotateAnimation();
   zooming();
@@ -62,6 +73,7 @@ function move() {
       clearInterval(rotate);
       img.style.visibility = 'hidden';
       overlay(); //matrix city
+
       //reset the javascript
       setTimeout(function () {
         window.location = window.location.href;
@@ -80,8 +92,8 @@ function move() {
 //overlay matrix view with text for a few seconds
 function on() {
   document.getElementById("overlay").style.display = "block";
-  $(document.getElementById("p1")).delay(1000).fadeIn();
-  $(document.getElementById("p2")).delay(2000).fadeIn();
+  $('.welcome').hide();
+  $('.welcome').fadeIn(2000);
 }
 
 function off() {
