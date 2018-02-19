78e1b126f1c52fdcb610455464790da145be8ce8
Daniel Chernis <32421770+dch133@users.noreply.github.com>
2018-02-19 18:51:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ if (!isMobile()) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
+    this.period = parseInt(period, 5) || 2000;
     this.txt = '';
     this.tick();
     this.isDeleting = false;
